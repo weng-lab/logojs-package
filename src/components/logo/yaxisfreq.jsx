9f52c71dd6f329584c1024393abfbebbe21accef
hpratt <hpratt111@live.com>
2019-08-18 09:07:35 -0400

    fix error

--- src/components/logo/yaxisfreq.jsx
+++ src/components/logo/yaxisfreq.jsx
@@ -11,12 +11,12 @@ import { xrange } from '../../common/utils';
  * @prop width the width of the logo relative to the containing SVG.
  */
 const YAxisFrequency = ({ ticks, transform, height, width }) => {
-    const ticks = xrange(ticks + 1).map(x => x / ticks);
+    const _ticks = xrange(ticks + 1).map(x => x / ticks);
     return (
 	<g transform={transform}>
 	  <rect height={height} width={4} x={width + 1} y="0"
 	        fill="#000000" />
-	  {ticks.map( x => (
+	  {_ticks.map( x => (
 	      <g key={x} transform={"translate(0," + (height - Math.floor(x * height)) + ")"}>
 		<text x={width - 10} textAnchor="end" y="4" fontSize="18">
                   {(x + '').substring(0, 4)}
