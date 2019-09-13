51e69a5cf51c3f5a775147e38b07d1c7d1255e3d
hpratt <hpratt111@live.com>
2019-09-13 13:11:16 -0400

    drop dead code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ProteinLogo, { ProteinAlphabet } from './components/logo/proteinlogo';
 import DNALogo, { DNAAlphabet } from './components/logo/dnalogo';
 import RNALogo, { RNAAlphabet } from './components/logo/rnalogo';
 import CompleteLogo, { CompleteAlphabet } from './components/logo/completelogo';
-import TestLogo from './components/logo/testlogo';
 import { xrange, disymbolAlphabet, loadGlyphComponents } from './common/utils';
 import { embedDNALogo, embedRNALogo, embedProteinLogo, embedLogo, embedRawLogo } from './js/embed';
 import { INFORMATION_CONTENT, FREQUENCY } from './common/utils';
